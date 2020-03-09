e90591c860b973540e5973cafb06f5f2636a46ef
Mohamed Hasan <mnt@plusmnt.com>
2020-03-10 01:01:43 +0300

    add server

---
+++ server.py
@@ -0,0 +1,18 @@
+from data_source import get_data
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse
+import json
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        self.send_response(200)
+        self.end_headers()
+        data=get_data()
+        self.wfile.write(json.dumps(data).encode())
+        return
+
+if __name__ == '__main__':
+    server = HTTPServer(('localhost', 8000), RequestHandler)
+    print('Starting server at http://localhost:8000')
+    server.serve_forever()
