f6d787c720f57d3d66fe4ae730d2d5449100bac3
Mohamed Hasan <mnt@plusmnt.com>
2020-03-10 00:46:42 +0300

    analyze html file

--- count.py
+++ count.py
@@ -1,1 +1,18 @@
-html=open("demo.html","r");
+from bs4 import BeautifulSoup
+
+html=open("demo.html","r").read();
+#parse html
+soup=BeautifulSoup(html, "html5lib")
+#get statistics table
+table=soup.findAll("table")[0]
+#get total check 
+total_check=table.findAll("tr")[0].findAll("span")[0].text
+negative_cases=table.findAll("td")[0].findAll("span")[0].text
+existing_cases=table.findAll("td")[1].findAll("span")[0].text
+stable_existing_cases=table.findAll("td")[3].findAll("span")[0].text
+critical_existing_cases=table.findAll("td")[4].findAll("span")[0].text
+outside_cases=table.findAll("td")[5].findAll("span")[0].text
+cases_around_outside_cases=table.findAll("td")[5].findAll("span")[1].text
+local_cases=table.findAll("td")[5].findAll("span")[2].text
+recovered_cases=table.findAll("td")[6].findAll("span")[0].text
+print(recovered_cases)
