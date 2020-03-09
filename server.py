eb4e3a8a2bf11e245007481f81f79af8decda6f4
Mohamed Hasan <mnt@plusmnt.com>
2020-03-10 01:15:09 +0300

    working http.server

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python3
 from data_source import get_data
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
 import json
 
+#https://gist.github.com/bsingr/a5ef6834524e82270154a9a72950c842
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse(self.path)
         self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
         self.end_headers()
         data=get_data()
         self.wfile.write(json.dumps(data).encode())
